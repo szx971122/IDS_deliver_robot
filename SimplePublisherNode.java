5ae1a42d2fd64736ea79e3602e92333eb95806a1
szx971122 <291368069@qq.com>
2021-02-05 12:04:57 +0100

    update node.java

--- SimplePublisherNode.java
+++ SimplePublisherNode.java
@@ -37,8 +37,20 @@ public class SimplePublisherNode extends AbstractNodeMain implements NodeMain {
     private static final String TAG = SimplePublisherNode.class.getSimpleName();
 
     public boolean is_goal_confirmed;
-    public boolean is_robot_completed_task;
     public boolean is_robot_reached_target;
+    public boolean is_app_completed_interaction;
+
+    /*
+      app_status = 0: FREE
+                   1: SETTING SEND POINT AND PICKING UP
+                   2: CONFIRM SEND POINT
+                   3: SETTING DESTINATION
+                   4: CONFIRM DESTINATION
+                   5: ARRIVED AND DELIVER
+                   6: CONFIRM RECEIVE
+     */
+    public int app_status;
+
     public String strGoalId;
 
     @Override
@@ -49,12 +61,12 @@ public class SimplePublisherNode extends AbstractNodeMain implements NodeMain {
     @Override
     public void onStart(ConnectedNode connectedNode) {
 
-        is_goal_confirmed = false;
-        is_robot_completed_task = false;
-        is_robot_reached_target = false;
+        app_status = 0;
+        is_robot_reached_target = true;
+        is_app_completed_interaction = true;
 
         final Publisher<std_msgs.String> pubGoalId = connectedNode.newPublisher(GraphName.of("service_goal_id"), std_msgs.String._TYPE);
-        final Publisher<std_msgs.UInt8> pubTaskStatus = connectedNode.newPublisher(GraphName.of("service_status"), UInt8._TYPE);
+        final Publisher<std_msgs.UInt8> pubAppStatus = connectedNode.newPublisher(GraphName.of("app_status"), UInt8._TYPE);
 
         final Subscriber<std_msgs.UInt8> subServiceStatus = connectedNode.newSubscriber(GraphName.of("turtlebot3_service_status"), UInt8._TYPE);
 
@@ -70,18 +82,53 @@ public class SimplePublisherNode extends AbstractNodeMain implements NodeMain {
         final CancellableLoop loop = new CancellableLoop() {
             @Override
             protected void loop() throws InterruptedException {
+//                if (is_robot_reached_target && is_app_completed_interaction){
+//                    if (robot_status == 0){
+//
+//                    }
+//                    else if (robot_status == 1){
+//                        //Called
+//                        std_msgs.String str = pubGoalId.newMessage();
+//                        str.setData(strGoalId);
+//                        pubGoalId.publish(str);
+//                        is_robot_reached_target = false;
+//                        robot_status = 2;
+//                    }
+//                    else if (robot_status == 2){
+//                        //Arrived at send point
+//                        is_app_completed_interaction = false;
+//                        // it will be reset to true when the client press confirm
+//                        robot_status = 3;
+//                    }
+//                    else if (robot_status == 3){
+//                        //Departure
+//                        std_msgs.String str = pubGoalId.newMessage();
+//                        str.setData(strGoalId);
+//                        pubGoalId.publish(str);
+//                        is_robot_reached_target = false;
+//                        robot_status = 4;
+//                    }
+//                    else if (robot_status == 4){
+//                        //Arrived at destination
+//                        is_app_completed_interaction = false;
+//                        // it will be reset to true when the client confirm receive
+//                        robot_status = 5;
+//                    }
+//                    else if (robot_status == 5){
+//
+//                    }
+//                }
+
                 if (is_goal_confirmed) {
                     std_msgs.String str = pubGoalId.newMessage();
                     str.setData(strGoalId);
                     pubGoalId.publish(str);
                     is_goal_confirmed = false;
-                    is_robot_completed_task = false;
-                    is_robot_reached_target = false;
                 }
-                if(is_robot_completed_task) {
-                    std_msgs.UInt8 status = pubTaskStatus.newMessage();
-                    status.setData((byte) 1);
-                    pubTaskStatus.publish(status);
+                if (is_app_completed_interaction) {
+                    std_msgs.UInt8 status = pubAppStatus.newMessage();
+                    status.setData((byte) app_status);
+                    is_app_completed_interaction = false;
                 }
 
                 // go to sleep for one second
